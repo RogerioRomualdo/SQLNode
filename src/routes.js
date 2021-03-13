cf96f073804188c457e5ecb438ea5faa1aabcc5f
RogerioRomualdo <rogerioromualdo12@gmail.com>
2021-03-13 18:19:12 -0300

    Added index route to show all users

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userController = require("./controllers/userController");
 const routes = express.Router();
 
+routes.get("/users", userController.index);
 routes.post("/users", userController.store);
 
 module.exports = routes;
