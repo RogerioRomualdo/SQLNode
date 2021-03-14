521f71a87f2415e831f5d92198df691d698e7691
RogerioRomualdo <rogerioromualdo12@gmail.com>
2021-03-13 21:43:43 -0300

    Added a route to list an adress based on user_id

--- src/controllers/addressController.js
+++ src/controllers/addressController.js
@@ -2,6 +2,21 @@ const Address = require("../models/Address");
 const User = require("../models/User");
 
 module.exports = {
+  async index(req, res) {
+    // const { user_id } = req.params;
+    // if (!user) {
+    //   return res.status(400).json({ error: "User not found" });
+    // }
+    // const addresses = await Address.findByPk(user_id);
+    // return res.json(addresses);
+    const { user_id } = req.params;
+
+    const user = await User.findByPk(user_id);
+    const addresses = await Address.findAll();
+
+    return res.json(addresses);
+  },
+
   async store(req, res) {
     const { user_id } = req.params;
     const { zipcode, street, number } = req.body;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const userController = require("./controllers/userController");
-const addresController = require("./controllers/addressController.js");
+const addressController = require("./controllers/addressController.js");
 
 const routes = express.Router();
 
 routes.get("/users", userController.index);
 routes.post("/users", userController.store);
 
-routes.post("/users/:user_id/addresses", addresController.store);
+routes.get("/users/:user_id/addresses", addressController.index);
+routes.post("/users/:user_id/addresses", addressController.store);
 
 module.exports = routes;
